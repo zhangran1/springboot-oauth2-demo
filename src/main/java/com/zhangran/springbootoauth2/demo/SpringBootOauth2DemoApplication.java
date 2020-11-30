db4d4f1eb23c52ae83527acf3f78d6b5cb373d5f
Zhang Ran <zhang.ran@stengg.com>
2020-11-30 10:03:23 +0800

    Initialize SprintBoot Project

---
+++ src/main/java/com/zhangran/springbootoauth2/demo/SpringBootOauth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhangran.springbootoauth2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangran/springbootoauth2/demo/SpringBootOauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangran.springbootoauth2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
