aedbaddcbfc2a031fc0c80d2e861a27b047893eb
Zhang Ran <zhang.ran@stengg.com>
2020-11-30 16:21:27 +0800

    Add RestController, make home page public

--- src/main/java/com/zhangran/springbootoauth2/demo/SpringBootOauth2DemoApplication.java
+++ src/main/java/com/zhangran/springbootoauth2/demo/SpringBootOauth2DemoApplication.java
@@ -1,13 +1,43 @@
 package com.zhangran.springbootoauth2.demo;
 
+import java.util.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.annotation.*;
+import org.springframework.security.oauth2.core.user.*;
+import org.springframework.security.web.authentication.*;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-public class SpringBootOauth2DemoApplication {
+@RestController
+public class SpringBootOauth2DemoApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootOauth2DemoApplication.class, args);
+    }
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login();
+        // @formatter:on
     }
 
 }
